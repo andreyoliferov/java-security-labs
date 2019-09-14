61c821fa452f2abb7b537f7963b043938df9bada
Andrey Oliferov <a.oliferov@gmail.com>
2019-09-14 20:17:18 +0700

    Защита от IDOR, аутентификация для метода

--- labs/lab-idor/src/main/java/edu/otib/lab_idor/WebSecurityConfig.java
+++ labs/lab-idor/src/main/java/edu/otib/lab_idor/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package edu.otib.lab_idor;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,6 +13,7 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true) /* поддержка авторизации на методах и классах. защита от IDOR */
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
     @Override
     protected void configure(HttpSecurity http) throws Exception {
--- labs/lab-idor/src/main/java/edu/otib/lab_idor/controller/PaymentController.java
+++ labs/lab-idor/src/main/java/edu/otib/lab_idor/controller/PaymentController.java
@@ -3,6 +3,7 @@ package edu.otib.lab_idor.controller;
 import edu.otib.lab_idor.entity.Payment;
 import edu.otib.lab_idor.service.IPaymentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,6 +18,7 @@ public class PaymentController {
     IPaymentService paymentService;
 
     @GetMapping(value = {"/", "/payments"})
+    @PostAuthorize("#model[payments].get(0).getUser() == authentication.name")
     public String getUserPayments(Model model, Authentication authentication) {
         List<Payment> userPayments = paymentService.getPaymentsByUser(authentication.getName());
         model.addAttribute("payments", userPayments);
@@ -24,6 +26,8 @@ public class PaymentController {
     }
 
     @GetMapping("/payments/{id}")
+    @PostAuthorize("#model[payment].user == authentication.name") /* защита от IDOR, аутентификация для объекта в model */
+    /*@PostAuthorize("returnObject.user == authentication.name")*/
     public String getPaymentById(@PathVariable("id") Integer id, Model model) {
         Payment payment = paymentService.getPaymentById(id);
         model.addAttribute("payment", payment);
